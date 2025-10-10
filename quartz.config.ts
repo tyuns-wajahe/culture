9d39e7142f7bd0ab834734628cd41bd9322b4d0c
Anurag-Shah <anuragshah2000@gmail.com>
2025-10-10 19:18:14 -0400

    workflow fix mayber

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://tyuns-wajahe.github.io/culture",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
